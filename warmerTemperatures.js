b5a0a0fae15ef105eaa6335d185c1d6a65b6ec7b
patrickpiedad <patrickpiedad@gmail.com>
2024-06-28 09:53:19 +0200

    completed cassidoo interview question of the week, code refactored

--- warmerTemperatures.js
+++ warmerTemperatures.js
@@ -23,21 +23,31 @@
 // is greater than next element, push 0 into ans Array)
 
 //SOLUTION
+function warmerTemperatures(temperatures) {
+	// create ansArray to house newArray
+	let ansArray = []
 
-function warmerTemperatures(temperatures){
-    let ansArray = []
-    for (let i = 0; i < temperatures.length; i++){
-        for (let j = 0; j < temperatures.length; i++){
-            if (temperatures[i] > temperatures[j]){
-                break
-            }else if (temperatures[i] < temperatures[j]){
-                ansArray.push(j - i)
-            } else if (temperatures[i] > temperatures[j] && j-1 === temperatures.length){
-                ansArray.push(0)
-            }
-        }
-    }
-    return ansArray
+	// setup outer loop
+	for (let i = 0; i <= temperatures.length; i++) {
+		// setup inner loop, this lets the outer loop stay on one value, while the inner loop can cycle through the rest of the array
+		for (let j = i + 1; j - 1 < temperatures.length; j++) {
+			//check for when inner loop is on the last value of the array, if it is on the last value of the array, there are no more days to compare to (does not get warmer), so push 0
+			// when true, execute code block, then break out of inner loop and execute next outer loop iteration
+			if (j === temperatures.length) {
+				ansArray.push(0)
+				break
+				// check if the current temp is less than the next compared temp (meaning it gets warmer), then push the index of that element minus the index of current
+				// when true, execute code block, then break out of inner loop and execute next outer loop iteration
+			} else if (temperatures[i] < temperatures[j]) {
+				ansArray.push(j - i)
+				break
+				// if the current temp is the same as the compared temp, then it does not meet any of the conditions, and inner loop continues to next inner loop iteration
+			}
+		}
+	}
+	return ansArray
 }
 
+console.log(warmerTemperatures([70, 70, 70, 75]))
+console.log(warmerTemperatures([90, 80, 70, 60]))
 console.log(warmerTemperatures([73, 74, 75, 71, 69, 72, 76, 73]))
